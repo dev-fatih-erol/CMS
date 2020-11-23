5cefa6f99bccae75bde7dec84d7544b306a1bf02
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-11-24 02:14:20 +0300

    .

--- CMS.Admin.Web/Startup.cs
+++ CMS.Admin.Web/Startup.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CMS.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +10,21 @@ namespace CMS.Admin.Web
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                     options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"),
+                     b => b.MigrationsAssembly("CMS.Admin.Web")));
+
             services.AddControllersWithViews();
         }
 
---
+++ CMS.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using CMS.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+
+        public DbSet<Chief> Chiefs { get; set; }
+
+        public DbSet<House> Houses { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Admin>().ToTable("Admin");
+
+            modelBuilder.Entity<Chief>().ToTable("Chief");
+
+            modelBuilder.Entity<House>().ToTable("House");
+
+            modelBuilder.Entity<Region>().ToTable("Region");
+        }
+    }
+}
