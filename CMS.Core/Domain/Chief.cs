4453d0cfb70a3e6ab47a4d7d40c5edb37fcc4df9
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-11-24 01:28:16 +0300

    .

---
+++ CMS.Core/Domain/Chief.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CMS.Core.Domain
+{
+    public class Chief
+    {
+        public int Id { get; set; }
+
+        public string IdentificationNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+
+        public int RegionId { get; set; }
+
+        public Region Region { get; set; }
+
+
+        public List<House> House { get; set; }
+    }
+}
---
+++ CMS.Core/Domain/House.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CMS.Core.Domain
+{
+    public class House
+    {
+        public int Id { get; set; }
+
+        public string IdentificationNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Address { get; set; }
+
+        public string CounterNumber { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+
+        public int ChiefId { get; set; }
+
+        public Chief Chief { get; set; }
+    }
+}
