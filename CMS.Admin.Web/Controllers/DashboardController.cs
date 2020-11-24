d4a9e5dcfa5e5c35ef57a597a2943e9403c2e156
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-11-24 18:47:04 +0300

    .

---
+++ CMS.Admin.Web/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Admin.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        [Route("Dashboard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
