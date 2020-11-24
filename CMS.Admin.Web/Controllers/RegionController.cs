28870490b0ed602a10cf37d702b3019d37a3d825
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-11-24 23:22:07 +0300

    .

---
+++ CMS.Admin.Web/Controllers/RegionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Admin.Web.Controllers
+{
+    public class RegionController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        [Route("Region/Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        [Route("Region/Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
