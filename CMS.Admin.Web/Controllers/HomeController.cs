6ad1ad9ac3db51146b4addc74f9b87ebba483ce0
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-11-24 18:10:18 +0300

    .

--- CMS.Admin.Web/Controllers/HomeController.cs
+++ CMS.Admin.Web/Controllers/HomeController.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CMS.Admin.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CMS.Admin.Web.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         [Route("")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(IndexViewModel model)
+        {
+            ModelState.AddModelError(string.Empty, "Girdiğin kullanıcı adı veya şifre hiçbir hesapla eşleşmiyor. Hesap bilgilerinizi unuttuysanız lütfen bizimle iletişime geçin.");
+
+            return View(model);
         }
     }
 }
