22be835016dc8280bb4f4b5cd5bc3516fa0877cd
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-11-26 04:50:45 +0300

    .

---
+++ CMS.Chief.Web/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using CMS.Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Chief.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IChiefService _chiefService;
+
+        public DashboardController(
+            IChiefService chiefService)
+        {
+            _chiefService = chiefService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("Dashboard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
