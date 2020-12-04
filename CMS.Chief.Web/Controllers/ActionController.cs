f6937f8e1a76b51ea779ec0084a5257e0e0c34f5
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-12-04 21:26:04 +0300

    .

---
+++ CMS.Chief.Web/Controllers/ActionController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using CMS.Chief.Web.Extensions;
+using CMS.Chief.Web.Models;
+using CMS.Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Chief.Web.Controllers
+{
+    public class ActionController : Controller
+    {
+        private readonly IHouseService _houseService;
+
+        private readonly IActionService _actionService;
+
+        public ActionController(IHouseService houseService,
+            IActionService actionService)
+        {
+            _houseService = houseService;
+
+            _actionService = actionService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("House/{houseId:int}/Action/Payment")]
+        public IActionResult Payment(int houseId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        [Route("House/{houseId:int}/Action/Payment")]
+        public IActionResult Payment(int houseId, PaymentViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var house = _houseService.GetById(houseId, User.Identity.GetId());
+
+            if (house != null)
+            {
+                _actionService.Create(new Core.Domain.Action
+                {
+                    Endeks = 0,
+                    Price = model.Price,
+                    Description = model.Description,
+                    Type = Core.Domain.Type.Payment,
+                    CreatedDate = DateTime.Now,
+                    HouseId = house.Id
+                });
+
+                return View("Success");
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ CMS.Chief.Web/Models/PaymentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Chief.Web.Models
+{
+    public class PaymentViewModel
+    {
+        [Required(ErrorMessage = "Lütfen fiyat girin")]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
